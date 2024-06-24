87fd86c85a8c8e6890bc53989db318f9570f6a92
hoywu <wu_git@outlook.com>
2024-06-24 19:39:11 +0800

    chore: update tips and label

--- src/views/ImportView.vue
+++ src/views/ImportView.vue
@@ -23,17 +23,18 @@ const s = useStateStore();
 
 const qtype = ref(0);
 const tips = [
-  // 选择题提示
+  // 选择判断题提示
   `- 题面以序号开头 (可后跟多行)
 - 选项与题面换行分隔，选项行以字母开头 (可单/多行)
 - 答案单独成行、或以字母形式填在题面括号中
-- 解析行以 “答案解析:” 或 “解析:” 开头 (可选)"`,
+- 解析行以 “答案解析:” 或 “解析:” 开头 (可选)"
+- 判断题答案以文字/符号形式填在括号中`,
   // 填空题提示
   `- 题面以序号开头 (可后跟多行)
 - 包含任意非空白字符的括号视为一个填空`,
   // 简答题提示
   `- 题面以序号开头 (可后跟多行)
-- 答案单独成行，以 “答案:/答:” 等标识开头`,
+- 答案单独成行，若题目有多行，答案需以 “答案:/答:” 等标识开头`,
 ];
 const parse: Array<Function> = [parseSelectQuestion, parseBlankQuestion, parseSAQ];
 
@@ -69,22 +70,22 @@ async function importQuestion(append: boolean) {
     <el-input v-model="nameInput" placeholder="题库名称 (覆盖导入时有效)" />
 
     <el-radio-group v-model="qtype">
-      <el-radio :value="0">选择题</el-radio>
+      <el-radio :value="0">选择判断题</el-radio>
       <el-radio :value="1">填空题</el-radio>
       <el-radio :value="2">简答题</el-radio>
     </el-radio-group>
 
     <el-input
       v-model="importQuestionInput"
-      :autosize="{ minRows: 6, maxRows: 6 }"
+      :autosize="{ minRows: 7, maxRows: 30 }"
       type="textarea"
       :placeholder="'在此粘贴题库\n' + tips[qtype]"
     />
     <el-input
       v-model="customFilter"
-      :autosize="{ minRows: 1, maxRows: 6 }"
+      :autosize="{ minRows: 1, maxRows: 10 }"
       type="textarea"
-      placeholder="自定义行过滤正则表达式 (每行一条)"
+      placeholder="自定义行过滤正则表达式 (每行一条) 匹配的行将被忽略"
     />
   </div>
 
